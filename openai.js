145f1e4e227b230cab8110e95176e384c3cf476e
Marcos-Yogi <marcoshiroshi2005@gmail.com>
2025-03-31 11:42:54 -0300

    Update openai.js

--- openai.js
+++ openai.js
@@ -1,4 +1,4 @@
-import { Configuration, OpenAIApi } from "./node_modules/openai/index";
+import { Configuration, OpenAIApi } from "./node_modules/openai/index.ts";
 
 const configuration = new Configuration({
     apiKey: process.env.API_KEY
