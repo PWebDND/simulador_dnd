409435cfa0895b32d990f30954a71fd61921caea
Marcos-Yogi <marcoshiroshi2005@gmail.com>
2025-03-31 12:10:52 -0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,1 +1,12 @@
+async function fetchAIResponse() {
+  try {
+    const response = await fetch("/.netlify/functions/openai.js", {
+      method: "POST",
+      request 
+    });
+    fetchAIResponse();
+  }
+}
+fetchAIResponse();
 
+  
